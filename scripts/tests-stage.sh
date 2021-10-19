8e51b15f5c20ef9414193543da96c57984800817
Alex Shaver <alexshaver@gmail.com>
2021-10-19 18:36:44 -0500

    Quotes around variables

--- scripts/tests-stage.sh
+++ scripts/tests-stage.sh
@@ -6,12 +6,12 @@ fi
 mkdir -p "${LOCAL_COVERAGE_DIR}"
 
 docker run \
-    -v "${PWD}":${CONTAINER_REPO_DIR} \
-    -v "${LOCAL_BUILD_DIR}":${CONTAINER_BUILD_DIR} \
-    -v "${LOCAL_COVERAGE_DIR}":${CONTAINER_COVERAGE_DIR} \
-    -e SOURCE_DIR=${CONTAINER_SOURCE_DIR} \
-    -e BUILD_DIR=${CONTAINER_BUILD_DIR} \
-    -e COVERAGE_DIR=${CONTAINER_COVERAGE_DIR} \
-    -w ${CONTAINER_BUILD_DIR} \
+    -v "${PWD}":"${CONTAINER_REPO_DIR}" \
+    -v "${LOCAL_BUILD_DIR}":"${CONTAINER_BUILD_DIR}" \
+    -v "${LOCAL_COVERAGE_DIR}":"${CONTAINER_COVERAGE_DIR}" \
+    -e SOURCE_DIR="${CONTAINER_SOURCE_DIR}" \
+    -e BUILD_DIR="${CONTAINER_BUILD_DIR}" \
+    -e COVERAGE_DIR="${CONTAINER_COVERAGE_DIR}" \
+    -w "${CONTAINER_BUILD_DIR}" \
     --entrypoint "unit-test.sh" \
     shavera/ci-unit-test
