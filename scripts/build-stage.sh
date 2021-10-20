825d24b4cb41ed2ebb2efe3f7cd7809b86d51a7f
Alex Shaver <alexshaver@gmail.com>
2021-10-20 10:50:55 -0500

    Debug print build dir contents
    Not sure if maybe travis is not persisting
    data through stages?

--- scripts/build-stage.sh
+++ scripts/build-stage.sh
@@ -4,9 +4,13 @@ if [[ -d "${LOCAL_BUILD_DIR}" ]]; then
   rm -rf "${LOCAL_BUILD_DIR}"
 fi
 mkdir -p "${LOCAL_BUILD_DIR}"
+echo "Build stage: ${LOCAL_BUILD_DIR} -- ${CONTAINER_BUILD_DIR}"
 docker run \
     -v "${PWD}":${CONTAINER_REPO_DIR} \
     -v BuildDir:${CONTAINER_BUILD_DIR} \
     -e SOURCE_DIR=${CONTAINER_SOURCE_DIR} \
     -e BUILD_DIR=${CONTAINER_BUILD_DIR} \
    shavera/ci-cmake-builder
+
+echo "Build complete: local build dir contents: "
+ls "${LOCAL_BUILD_DIR}"
