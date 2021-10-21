5218188a48f5d31f4017fadf2d99cb4588ffd420
Alexander Shaver <alexshaver@gmail.com>
2021-10-20 20:51:51 -0500

    Simplify code back to simple case
    Initial branch is just setting up the CI.
    This simplifies the project back now that
    the CI is all set up correctly.

--- cpp/calculator/Dummy.cpp
+++ cpp/calculator/Dummy.cpp
@@ -8,20 +8,4 @@ std::string Dummy::helloWorld() const{
     return "Hello World!";
 }
 
-std::string Dummy::helloWho(int index) const {
-  if(index < 0){
-    return "Hello You.";
-  }
-  if((0 == index % 2 && index < 20) || 3 == index){
-    if(index < 10){
-      return "Hello alpha.";
-    }
-    if(index > 50){
-      return "No way you get here.";
-    }
-    return "Hello beta.";
-  }
-  return "Hello gamma.";
-}
-
 } // namespace orbit_calculator
