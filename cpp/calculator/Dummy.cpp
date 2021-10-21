43dc9bba455f11c3050c2f68d341a109b7b38182
Alex Shaver <alexshaver@gmail.com>
2021-10-21 08:21:15 -0500

    Add an uninitialized variable
    Should violate static analysis so we
    can start configuring that.

--- cpp/calculator/Dummy.cpp
+++ cpp/calculator/Dummy.cpp
@@ -9,16 +9,19 @@ std::string Dummy::helloWorld() const{
 }
 
 std::string Dummy::helloWho(int index) const {
+  int offset;
   if(index < 0){
     return "Hello You.";
   }
   if((0 == index % 2 && index < 20) || 3 == index){
+    offset = 7;
     if(index < 10){
       return "Hello alpha.";
     }
     if(index > 50){
       return "No way you get here.";
     }
+    std::cout << offset <<std::endl;
     return "Hello beta.";
   }
   return "Hello gamma.";
